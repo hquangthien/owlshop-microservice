0193fd4ad107a578392cef331e53378db4742b63
thien.hoang <hquangthien@gmail.com>
2021-05-30 08:34:55 +0700

    Ordering service: implement services/repos for infrastructure project

---
+++ src/Services/Ordering/Ordering.Infrastructure/Mail/EmailService.cs
@@ -0,0 +1,49 @@
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Contracts.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace Ordering.Infrastructure.Mail
+{
+    public class EmailService : IEmailService
+    {
+        private EmailSettings EmailSettings { get; }
+        private ILogger<EmailService> Logger { get; }
+
+        public EmailService(IOptions<EmailSettings> mailSettings, ILogger<EmailService> logger)
+        {
+            EmailSettings = mailSettings.Value;
+            Logger = logger;
+        }
+
+        public async Task<bool> SendEmail(Email email)
+        {
+            var client = new SendGridClient(EmailSettings.ApiKey);
+
+            var subject = email.Subject;
+            var to = new EmailAddress(email.To);
+            var emailBody = email.Body;
+
+            var from = new EmailAddress
+            {
+                Email = EmailSettings.FromAddress,
+                Name = EmailSettings.FromName
+            };
+
+            var sendGridMessage = MailHelper.CreateSingleEmail(from, to, subject, emailBody, emailBody);
+            var response = await client.SendEmailAsync(sendGridMessage);
+
+            Logger.LogInformation("Email sent.");
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Accepted || response.StatusCode == System.Net.HttpStatusCode.OK)
+                return true;
+
+            Logger.LogError("Email sending failed.");
+
+            return false;
+        }
+    }
+}
