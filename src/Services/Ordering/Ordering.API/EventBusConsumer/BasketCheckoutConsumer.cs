79366a343a050ad42ad8add7f5848bf29bd74982
thien.hoang <hquangthien@gmail.com>
2021-06-04 21:23:29 +0700

    OrderingAPI: add consumer

---
+++ src/Services/Ordering/Ordering.API/EventBusConsumer/BasketCheckoutConsumer.cs
@@ -0,0 +1,32 @@
+using System.Threading.Tasks;
+using AutoMapper;
+using EventBus.Messages.Events;
+using MassTransit;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+
+namespace Ordering.API.EventBusConsumer
+{
+    public class BasketCheckoutConsumer : IConsumer<BasketCheckoutEvent>
+    {
+        private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
+        private readonly ILogger<BasketCheckoutConsumer> _logger;
+
+        public BasketCheckoutConsumer(IMapper mapper, IMediator mediator, ILogger<BasketCheckoutConsumer> logger)
+        {
+            _mapper = mapper;
+            _mediator = mediator;
+            _logger = logger;
+        }
+        
+        public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        {
+            var checkoutCommand = _mapper.Map<CheckoutOrderCommand>(context.Message);
+            var result = await _mediator.Send(checkoutCommand);
+            
+            _logger.LogInformation($"BasketCheckoutEvent consumed successfully. Created Order Id - {result}");
+        }
+    }
+}
--- src/Services/Ordering/Ordering.API/Startup.cs
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -1,16 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using EventBus.Messages.Common;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Ordering.API.EventBusConsumer;
 using Ordering.Application;
 using Ordering.Infrastructure;
 
@@ -36,6 +32,23 @@ namespace Ordering.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "Ordering.API", Version = "v1"});
             });
+
+            services.AddAutoMapper(typeof(Startup));
+            services.AddScoped<BasketCheckoutConsumer>();
+            
+            services.AddMassTransit(config => {
+
+                config.AddConsumer<BasketCheckoutConsumer>();
+
+                config.UsingRabbitMq((ctx, cfg) => {
+                    cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+
+                    cfg.ReceiveEndpoint(EventBusConstants.BasketCheckoutQueue, c => {
+                        c.ConfigureConsumer<BasketCheckoutConsumer>(ctx);
+                    });
+                });
+            });
+            services.AddMassTransitHostedService();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
